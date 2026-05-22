61691ec810a29f0dd80e531697bdcf9fab6853b1
ian-robles67 <ian.robles@iest.edu.mx>
2026-05-21 19:31:11 -0600

    Add files via upload

---
+++ clicker.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Dim valor As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        valor += 1
+        Label1.Text = valor.ToString()
+    End Sub
+End Class
